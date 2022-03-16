1e2420308f232d202370a4703cbf4706bb8dc43c
tramypham2192 <61374685+tramypham2192@users.noreply.github.com>
2022-03-16 12:42:03 -0700

    Add files via upload

---
+++ Entry3.java
@@ -0,0 +1,13 @@
+package OOPReview;
+
+public class Entry3 {
+	public static void main(String[] args) {
+		Point begin = new Point(1, 1);
+		Point end = new Point(2, 2);
+		Line line1 = new Line(begin, end);
+		System.out.println(line1.getLength());
+
+		Line line2 = new Line(2, 3, 5, 4);
+		System.out.println(line2.getLength());
+	}
+}
